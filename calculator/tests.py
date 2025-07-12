3c6dbf88ed7bbc012bc32883446122dabb266cb3
Karson Bryant <karson.r.bryant@outlook.com>
2025-07-11 22:23:41 -0500

    added 'read file content' functions per CH2 l3 instructs

--- calculator/tests.py
+++ calculator/tests.py
@@ -1,10 +1,21 @@
 # tests.py
+import os
+import sys
 
-from functions.get_files_info import get_files_info
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"..")))
+
+from functions import get_file_content
 
 
+#print(get_file_content("calculator", "main"))
+#print(get_file_content("calculator","lorem.txt"))
 
-print(get_files_info("calculator", "."))
+
+#print(get_files_info("calculator", "."))
 #print(get_files_info("calculator", "pkg"))
 #print(get_files_info("calculator", "/bin"))
 #print(get_files_info("calculator", "../"))
+
+print(get_file_content(".", "main.py"))
+print(get_file_content(".", "pkg/calculator.py"))
+print(get_file_content(".", "/bin/cat"))
